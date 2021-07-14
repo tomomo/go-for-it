c30b9201c1f5d8720db99493ffdbb543685ee8f2
tomomo <eclairpark@gmail.com>
2021-07-14 12:57:30 +0900

    Mariadbとの接続確認

--- src/main.go
+++ src/main.go
@@ -1,34 +1,33 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	// engine := gin.Default()
-	// engine.LoadHTMLGlob("templates/*.html")
+	engine := gin.Default()
+	engine.LoadHTMLGlob("templates/*.html")
 
-	// engine.GET("/", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "index.html", gin.H{
-	// 		"title": "Go for it!",
-	// 	})
-	// })
-	// engine.Run(":3000")
-
-	router := gin.Default()
-	api := router.Group("api")
-	{
-		v1 := api.Group("v1")
-		{
-			v1.GET("/", func(c *gin.Context) {
-				c.JSON(http.StatusOK, gin.H{
-					"message": "hello",
-				})
-			})
-		}
+	db, err := sql.Open("mysql", "devuser:devpass@tcp(db:3306)/development")
+	if err != nil {
+		panic(err.Error())
 	}
-	router.Run(":3000")
+	defer db.Close()
 
+	var dbVersion string
+	db.QueryRow("SELECT VERSION()").Scan(&dbVersion)
+	fmt.Println("Connected to: ", dbVersion)
+
+	engine.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title":     "Go for it!!",
+			"dbVersion": dbVersion,
+		})
+	})
+	engine.Run(":3000")
 }
