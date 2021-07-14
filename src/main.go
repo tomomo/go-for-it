af5be94b70547a852315132f492a29c18d1fbcb0
tomomo <eclairpark@gmail.com>
2021-07-14 10:42:38 +0900

    ルーティングを書き換えてみる

--- src/main.go
+++ src/main.go
@@ -7,13 +7,28 @@ import (
 )
 
 func main() {
-	engine := gin.Default()
-	engine.LoadHTMLGlob("templates/*.html")
+	// engine := gin.Default()
+	// engine.LoadHTMLGlob("templates/*.html")
 
-	engine.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Go for it!",
-		})
-	})
-	engine.Run(":3000")
+	// engine.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.html", gin.H{
+	// 		"title": "Go for it!",
+	// 	})
+	// })
+	// engine.Run(":3000")
+
+	router := gin.Default()
+	api := router.Group("api")
+	{
+		v1 := api.Group("v1")
+		{
+			v1.GET("/", func(c *gin.Context) {
+				c.JSON(http.StatusOK, gin.H{
+					"message": "hello",
+				})
+			})
+		}
+	}
+	router.Run(":3000")
+
 }
